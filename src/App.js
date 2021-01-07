17fa52bf56df49b3f391b9ba0aa2a0b7eb147cd8
Eraj <erajkamdar15@gmail.com>
2021-01-07 14:17:51 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [val, setVal] = useState({})
+
+  useEffect(() => {
+
+    fetch("/.netlify/functions/hello?name=From React App")
+    .then(res=>res.json())
+    .then(obj=>setVal(obj))
+  })
   return (
     <div className="App">
      <h1>Hello World from React App
-       <br/> <br/> (UPDATED NOW)</h1>
+       <br/> <br/> (UPDATED NOW)
+       <br/> <br /> Then...</h1>
+       <h3>{val.message}</h3>
     </div>
   );
 }
